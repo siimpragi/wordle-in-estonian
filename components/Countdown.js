f1471afd3e83bb2812f82750975f2f2aebc82d1b
Siim Pragi <siim@pragi.ee>
2022-02-20 18:52:32 +0200

    Fix the counter

--- components/Countdown.js
+++ components/Countdown.js
@@ -2,18 +2,19 @@ import { useState, useEffect } from "react";
 
 const Countdown = ({ toTs }) => {
   const nowTs = Math.round(new Date().getTime() / 1000);
-  const [secondsRemaining, setSecondsRemaining] = useState(toTs - nowTs);
+  const [remaining, setRemaining] = useState(toTs - nowTs);
   useEffect(() => {
-    if (secondsRemaining > 0) {
-      setTimeout(() => {
-        setSecondsRemaining(secondsRemaining - 1);
+    if (remaining > 0) {
+      const timeoutID = setTimeout(() => {
+        setRemaining(remaining - 1);
       }, 1000);
+      return () => {
+        clearTimeout(timeoutID);
+      };
     }
-  }, [secondsRemaining]);
+  }, [remaining]);
 
-  const clockString = new Date(secondsRemaining * 1000)
-    .toISOString()
-    .slice(11, 19);
+  const clockString = new Date(remaining * 1000).toISOString().slice(11, 19);
 
   return <div>{clockString}</div>;
 };
